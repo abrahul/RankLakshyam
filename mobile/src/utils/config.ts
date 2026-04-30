d5fec2ab59cf86db40456ec6ba2e490423aaebb4
abrahul100 <abrahul100@gmail.com>
2026-04-30 11:54:36 +0530

    updated web dev id

--- mobile/src/utils/config.ts
+++ mobile/src/utils/config.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL = "https://rank-lakshyam.vercel.app";
 
 // Google OAuth - replace with your actual web client ID
-export const GOOGLE_WEB_CLIENT_ID = "755775791815-m702pdkj8le3jcgh45a4vig75c68hoo2.apps.googleusercontent.com";
+export const GOOGLE_WEB_CLIENT_ID = "755775791815-asilf0r89jvqkhnmchgptps6qvaa2hk8.apps.googleusercontent.com";
